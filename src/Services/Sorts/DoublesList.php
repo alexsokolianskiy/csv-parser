33f2b571b325283330ffe56bdcc1da2b5386cd42
Aleksandr Sokolyanskii <jorapobeditel@gmail.com>
2023-01-02 01:18:37 +0200

    found way to compare multi column object

--- src/Services/Sorts/DoublesList.php
+++ src/Services/Sorts/DoublesList.php
@@ -2,11 +2,13 @@
 
 namespace Alex\CsvParser\Services\Sorts;
 
+use Alex\CsvParser\Enums\UserColumn;
 use SplDoublyLinkedList;
+use Alex\CsvParser\Models\User;
 
 class DoublesList
 {
-    private SplDoublyLinkedList $list;
+    private array $sorted;
 
     public function __construct(
         private array $data,
@@ -16,20 +18,45 @@ class DoublesList
 
     private function emptyList(): void
     {
-        $this->list = new SplDoublyLinkedList();
+        $this->sorted = [];
+    }
+
+    private function getColumnGetterMap(string $column): ?string
+    {
+        $map = [
+            UserColumn::EMAIL->name => 'getEmail',
+            UserColumn::CARD->name => 'getCard',
+            UserColumn::PHONE->name => 'getPhone'
+        ];
+
+        return $map[$column] ?? null;
+    }
+
+    public function buildCompareMethod(array $columns): callable
+    {
+        return function (User $first, User $second) use ($columns) {
+            $optionsFirst = [];
+            $optionsSecond = [];
+            foreach ($columns as $column) {
+                $getterName = $this->getColumnGetterMap($column->name);
+                $optionsFirst[] = $first->$getterName();
+                $optionsSecond[] = $second->$getterName();
+            }
+            return $optionsFirst <=> $optionsSecond;
+        };
     }
 
     private function makeSortBy(array $columns = []): void
     {
         $this->emptyList();
-        foreach ($this->data as $user) {
-            // var_dump($user);
-        }
+        $dataCopy = $this->data;
+        usort($dataCopy, $this->buildCompareMethod($columns));
+        $this->sorted = $dataCopy;
     }
 
     public function findDoubles(array $columns = []): array
     {
         $this->makeSortBy($columns);
-        return [];
+        return $this->sorted;
     }
 }
